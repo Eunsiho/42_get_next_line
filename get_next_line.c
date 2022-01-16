e1cbec86f43f0734becf02402fdd92e150c88194
eunsiho <versailles@korea.ac.kr>
2022-01-16 22:46:21 +0900

    1

---
+++ get_next_line.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hogkim <hogkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/05 23:02:19 by hogkim            #+#    #+#             */
+/*   Updated: 2022/01/13 17:52:00 by hogkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_restart(char *temp)
+{
+	size_t	i;
+
+	i = 0;
+	while (temp[i])
+	{
+		if (temp[i] = '\n')
+			break ;
+		i++;
+	}
+	temp = temp[i + 1];
+	return (temp);
+}
+
+char	*ft_cut_temp(char *temp, char *line)
+{
+	size_t	i;
+
+	i = 0;
+	while (temp[i])
+	{
+		if (temp[i] = '\n')
+			break ;
+		i++;
+	}
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	ft_strlcpy(line, temp, i + 2);
+	return (line);
+}
+
+char	*ft_read_till_line(int fd, char *temp)
+{
+	char	*buffer;
+	ssize_t	n;
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	while ((n >= 1) && ft_no_newline(buffer))
+	{
+		n = read(fd, buffer, BUFFER_SIZE);
+		if (n == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[n] = 0;
+		ft_strjoin(temp, buffer);
+	}
+	free(buffer);
+	return (temp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*temp;
+	char		*line;
+
+	ft_read_till_line(fd, temp);
+	if (ft_no_newline(temp))
+		return (temp);
+	ft_cut_temp(temp, line);
+	ft_restart(temp);
+	return (line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hogkim <hogkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 23:26:06 by hogkim            #+#    #+#             */
+/*   Updated: 2022/01/13 17:52:01 by hogkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (src[i])
+		i++;
+	if (dstsize == 0)
+		return (i);
+	j = 0;
+	while (j < dstsize - 1 && j < i)
+	{
+		dst[j] = src[j];
+		j++;
+	}
+	dst[j] = 0;
+	return (i);
+}
+
+int	ft_no_newline(char *buffer)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(buffer))
+	{
+		if (buffer[i] == '\n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2);
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s1))
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < ft_strlen(s2))
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = 0;
+	return (str);
+}
